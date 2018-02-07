c6ef26af8c32530346bc33ba36e123c990d14edc
Nikola Kovacs <nikola.kovacs@gmail.com>
2018-02-07 10:59:17 +0100

    Wait for cassandra to start listening
    Otherwise cassandra-web fails to start.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,9 @@
 FROM ruby:2.5
 
+RUN apt-get update && \
+	apt-get install -y netcat --no-install-recommends && \
+	rm -rf /var/lib/apt/lists/*
+
 RUN gem install cassandra-web
 
 ADD entrypoint.sh /usr/local/bin/
--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,17 @@ if [ -z "$CASS_PORT" ]; then
 	CASS_PORT=9042
 fi
 
+echo "Waiting for Cassandra..."
+count=1
+while ! nc -vz "$HOST_IP" "$CASS_PORT"  > /dev/null 2>&1; do
+	count=$((count+1))
+	if [ $count -eq 30 ]; then
+		echo "Cassandra is still not listening, aborting"
+		exit 1
+	fi
+	sleep 1;
+done
+echo "Cassandra listening"
+
+
 cassandra-web --hosts "$HOST_IP" --port "$CASS_PORT" ${CASS_USERNAME:+"--username"} ${CASS_USERNAME:+"$CASS_USERNAME"} ${CASS_PASSWORD:+"--password"} ${CASS_PASSWORD:+"$CASS_PASSWORD"}
